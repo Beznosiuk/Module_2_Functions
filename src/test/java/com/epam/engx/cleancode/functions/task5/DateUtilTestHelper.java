7886dddfc85f01cb4a9726f2d7e28be6ee073a06
Iryna_Beznosiuk <iryna_beznosiuk@epam.com>
2021-08-17 10:09:18 +0300

    Module 2: changed code according to comments

--- src/test/java/com/epam/engx/cleancode/functions/task5/DateUtilTestHelper.java
+++ src/test/java/com/epam/engx/cleancode/functions/task5/DateUtilTestHelper.java
@@ -2,7 +2,11 @@ package com.epam.engx.cleancode.functions.task5;
 
 import java.util.Date;
 
-public class DateUtilTestHelper {
+public final class DateUtilTestHelper {
+
+    private DateUtilTestHelper() {
+        throw new UnsupportedOperationException("Do not instantiate this class, use statically.");
+    }
 
     public static Date getDirectlyIncrementedDate(DateService dateUtil, Date date) {
         return dateUtil.changeToMidnightDirectlyIncremented(date);
